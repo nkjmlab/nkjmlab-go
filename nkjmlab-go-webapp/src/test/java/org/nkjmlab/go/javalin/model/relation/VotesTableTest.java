59293264db6f7fd68e676a963eeda54f98065759
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2022-06-28 10:22:46 +0900

    VotesTableとProblemJsonReaderのテストを追加した．

---
+++ nkjmlab-go-webapp/src/test/java/org/nkjmlab/go/javalin/model/relation/VotesTableTest.java
@@ -0,0 +1,26 @@
+package org.nkjmlab.go.javalin.model.relation;
+
+import static org.assertj.core.api.Assertions.*;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+import org.nkjmlab.go.javalin.GoApplicationTestUtils;
+import org.nkjmlab.go.javalin.model.relation.VotesTable.Vote;
+import org.nkjmlab.go.javalin.model.relation.VotesTable.VoteResult;
+
+class VotesTableTest {
+
+  @Test
+  void testReadVoteResults() {
+    VotesTable vt = new VotesTable(GoApplicationTestUtils.getInMemoryDataSource());
+    vt.createTableIfNotExists();
+    long problemId = 1594221942280L;
+    vt.insert(new Vote("nkjm", problemId, "A", "40", "5588999", LocalDateTime.now()));
+    vt.insert(new Vote("nkjm2", problemId, "A", "40", "5588999", LocalDateTime.now()));
+    vt.insert(new Vote("nkjm3", problemId, "B", "50", "5588999", LocalDateTime.now()));
+
+    assertThat(vt.readVoteResults(problemId, null)).contains(new VoteResult("40", 2),
+        new VoteResult("50", 1));
+
+  }
+
+}
